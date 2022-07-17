a23e9f2e895b96b51bfaae6a172031c96589d7cd
SeyeongPark <lynn.yeong.102@gmail.com>
2022-07-16 21:26:39 -0400

    add design navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,15 +6,35 @@ function Navbar() {
     <>
         <nav className="navbar">
             <div className='navbar-container'>
-                <ul className='nav-menu'>
+                <ul className='navbar-nav'>
                   <li className='nav-item'>
-                    <a className='nav-links' href='/'>
-                      Home
+                    <a className='nav-link' href='/'>
+                      New
                     </a>
                   </li>
                   <li className='nav-item'>
-                    <a className='nav-links' href='/members'>
-                      Member
+                    <a className='nav-link' href='/'>
+                      Best Sellers
+                    </a>
+                  </li>
+                  <li className='nav-item'>
+                    <a className='nav-link' href='/'>
+                      Skincare
+                    </a>
+                  </li>
+                  <li className='nav-item'>
+                    <a className='nav-link' href='/'>
+                      Makeup
+                    </a>
+                  </li>
+                  <li className='nav-item'>
+                    <a className='nav-link' href='/'>
+                      Hair+Body
+                    </a>
+                  </li>
+                  <li className='nav-item'>
+                    <a className='nav-link' href='/'>
+                      Gifts
                     </a>
                   </li>
                 </ul>
